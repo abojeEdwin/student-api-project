b30f8d51b1646aa52a93643d177e90f2e690f1d4
Edwin Aboje <abojeedwin@gmail.com>
2025-04-16 21:46:57 +0100

    Testing Add Student Api

---
+++ Makefile
@@ -0,0 +1,9 @@
+.PHONY: install run test
+install:
+        .venv/bin/pip install -r requirements.txt
+
+run:
+        flask run
+
+test:
+        .venv/bin/pytest -v
---
+++ app/routes.py
@@ -0,0 +1,19 @@
+import logging
+from flask import request, jsonify
+from .student_models import db, Student
+
+logger = logging.getLogger(__name__)
+
+def init_routes(app):
+    @app.route("/api/v1/healthcheck", methods=["GET"])
+    def health_check():
+        return jsonify({"status": "ok"}),200
+
+    @app.route("/api/v1/students", methods=["POST"])
+    def add_students():
+        data = request.get_json()
+        student = Student(name=data["name"], age=data["age"])
+        db.session.add(student)
+        db.session.commit()
+        logger.info(f"Added student {student.name} added with ID {student.id}")
+        return jsonify({student.to_dict()}), 201
